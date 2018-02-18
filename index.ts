26c24b8713e5fc1bcb4aa5947890e19e06baa0cd
Arturo Albacete <aalbacetef@gmail.com>
2018-02-18 05:42:06 +0000

    Set project up to build a dist directory suitable for npm distribution

--- index.ts
+++
@@ -1,3 +0,0 @@
-export { Walker2D } from './src/walker2d';
-export { Walker2DStep8 } from './src/walker2d_8';
-export { SelfAvoidingWalker2D } from './src/self-avoiding-walker2d';
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { Walker2D } from './walkers/walker2d';
+export { Walker2DStep8 } from './walkers/walker2d_8';
+export { SelfAvoidingWalker2D } from './walkers/self-avoiding-walker2d';
